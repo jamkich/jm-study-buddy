cd568831354a5cecc30559e74bb12ce3b953b022
jamkich <jakub@MacBook-Air-Jakub.local>
2022-04-25 13:15:20 +0200

    fix bugs ulcomp 5

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
-import UsersList from "components/UsersList/UsersList";
+import React from 'react';
+import PropTypes from 'prop-types';
+import UsersList from 'components/UsersList/UsersList';
 
 const Root = () => (
   <>
