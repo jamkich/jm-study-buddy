5043b82d1aabb8b62c68153b5bb4abdb8003c135
Jakub <benqumichalik3@gmail.com>
2022-11-25 00:00:41 +0100

    Add useSearchBar

--- src/components/organisms/SearchBar/SearchBar.js
+++ src/components/organisms/SearchBar/SearchBar.js
@@ -1,29 +1,10 @@
-import { useState } from 'react';
 import { SearchBarWrapper, StatusInfo, SearchWrapper, SearchResults, SearchResultsItem } from './SearchBar.styles';
-import { useCombobox } from 'downshift';
-import debounce from 'lodash.debounce';
+import { useSearchBar } from 'hooks/useSearchBar';
 import { Input } from 'components/atoms/Input/Input';
-import { useFindStudentsMutation, openModal, setStudentData } from 'store';
-import { useDispatch } from 'react-redux';
 
 export const SearchBar = () => {
-  const [matchingStudents, setMatchingStudents] = useState([]);
-  const [findStudents] = useFindStudentsMutation();
-  const dispatch = useDispatch();
-
-  const wait = (amount = 0) => new Promise((resolve) => setTimeout(resolve, amount));
-
-  const getMatchingStudents = debounce(async (inputValue) => {
-    const {
-      data: { students },
-    } = await findStudents(inputValue);
-    setMatchingStudents(students);
-  }, 500);
-
-  const { isOpen, getMenuProps, getInputProps, getComboboxProps, highlightedIndex, getItemProps, selectedItem, setInputValue } = useCombobox({
-    items: matchingStudents,
-    onInputValueChange: getMatchingStudents,
-  });
+  const { isOpen, getMenuProps, getInputProps, getComboboxProps, highlightedIndex, getItemProps, selectedItem, onClickActions, matchingStudents } =
+    useSearchBar();
 
   return (
     <SearchBarWrapper>
@@ -44,14 +25,7 @@ export const SearchBar = () => {
                 key={item.id}
                 isHighlighted={highlightedIndex === index}
                 selectedItem={selectedItem === item}
-                onClick={async () => {
-                  dispatch(setStudentData(item));
-                  // modal is overwriting input with [object object] BUG
-                  // DONE used setTimeout with min. delay to make it work like should
-                  dispatch(openModal());
-                  await wait();
-                  setInputValue('');
-                }}
+                onClick={() => onClickActions(item)}
               >
                 {item.name}
               </SearchResultsItem>
---
+++ src/hooks/useSearchBar.js
@@ -0,0 +1,34 @@
+import { useCombobox } from 'downshift';
+import debounce from 'lodash.debounce';
+import { useDispatch } from 'react-redux';
+import { useFindStudentsMutation, openModal, setStudentData } from 'store';
+import { useState } from 'react';
+
+export const useSearchBar = () => {
+  const [matchingStudents, setMatchingStudents] = useState([]);
+  const dispatch = useDispatch();
+  const [findStudents] = useFindStudentsMutation();
+
+  const getMatchingStudents = debounce(async (inputValue) => {
+    const {
+      data: { students },
+    } = await findStudents(inputValue);
+    setMatchingStudents(students);
+  }, 500);
+
+  const { isOpen, getMenuProps, getInputProps, getComboboxProps, highlightedIndex, getItemProps, selectedItem, setInputValue } = useCombobox({
+    items: matchingStudents,
+    onInputValueChange: getMatchingStudents,
+  });
+
+  const onClickActions = async (data) => {
+    if (data) {
+      dispatch(setStudentData(data));
+      await dispatch(openModal());
+      setInputValue('');
+    }
+
+    return 'No data to use.';
+  };
+  return { isOpen, getMenuProps, getInputProps, getComboboxProps, highlightedIndex, getItemProps, selectedItem, onClickActions, matchingStudents };
+};
