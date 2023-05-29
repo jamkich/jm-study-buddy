49d57db6476bee0513fa1c74648530ffd85496d1
Jakub <benqumichalik3@gmail.com>
2023-05-29 12:41:01 +0200

    change Toggle content

--- src/providers/AppProviders.js
+++ src/providers/AppProviders.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { GlobalStyle } from 'assets/styles/GlobalStyle';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { darkTheme, lightTheme } from 'assets/styles/theme';
