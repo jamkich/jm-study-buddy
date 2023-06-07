72fe5f9c057d04d39669ea8a01c64cf45a7fa6dd
Jakub <benqumichalik3@gmail.com>
2023-06-07 13:29:18 +0200

    Update faker, Add TypeScript to project

--- src/mocks/db.js
+++ src/mocks/db.js
@@ -6,16 +6,16 @@ faker.seed(13);
 const groups = ['A', 'B', 'C'];
 const eventTypes = ['workshop', 'exam', 'lecture'];
 const getRandomValue = (array, index) => array[index];
-const getRandomAverage = () => faker.datatype.number({ min: 2, max: 5, precision: 0.1 });
+const getRandomAverage = () => faker.number.int({ min: 2, max: 5, precision: 0.1 });
 
 export const db = factory({
   student: {
-    id: primaryKey(faker.datatype.uuid),
-    name: () => faker.helpers.fake('{{name.firstName}} {{name.lastName}}'),
-    attendance: () => `${faker.datatype.number({ min: 0, max: 100 })}`,
+    id: primaryKey(faker.string.uuid),
+    name: () => faker.helpers.fake('{{person.firstName}} {{person.lastName}}'),
+    attendance: () => `${faker.number.int({ min: 0, max: 100 })}`,
     average: getRandomAverage,
-    group: () => getRandomValue(groups, faker.datatype.number({ min: 0, max: 2 })),
-    course: () => faker.helpers.fake('{{company.bsAdjective}} {{company.bsNoun}}'),
+    group: () => getRandomValue(groups, faker.number.int({ min: 0, max: 2 })),
+    course: () => faker.helpers.fake('{{company.buzzAdjective}} {{company.buzzNoun}}'),
     grades: () => [
       {
         subject: 'Business Philosophy',
@@ -35,10 +35,10 @@ export const db = factory({
     id: primaryKey(String),
   },
   event: {
-    id: primaryKey(faker.datatype.uuid),
-    type: () => getRandomValue(eventTypes, faker.datatype.number({ min: 0, max: 2 })),
-    group: () => getRandomValue(groups, faker.datatype.number({ min: 0, max: 2 })),
-    subject: () => faker.helpers.fake('{{company.bsAdjective}} {{company.bsNoun}}'),
+    id: primaryKey(faker.string.uuid),
+    type: () => getRandomValue(eventTypes, faker.number.int({ min: 0, max: 2 })),
+    group: () => getRandomValue(groups, faker.number.int({ min: 0, max: 2 })),
+    subject: () => faker.helpers.fake('{{company.buzzAdjective}} {{company.buzzNoun}}'),
     date: faker.date.soon,
   },
   teacher: {
@@ -49,7 +49,7 @@ export const db = factory({
   },
 
   note: {
-    id: primaryKey(faker.datatype.uuid),
+    id: primaryKey(faker.string.uuid),
     title: () => 'lorem ipsum dolor sit amet',
     content: () => 'lorem ipsum dolor sit amet',
   },
